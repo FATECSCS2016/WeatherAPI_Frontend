ab401de667a400d39579f15b628843a1eb2a460e
vitorsilvalima <vitor.lima2@fatec.sp.gov.br>
2017-05-22 01:16:55 -0300

    adding charts

--- src/app/pages/station/station-details/station-details.component.ts
+++ src/app/pages/station/station-details/station-details.component.ts
@@ -13,61 +13,86 @@ export class StationDetailsComponent implements OnInit {
 
   private sub: any;
   private device_id: string;
-  private chart: any;
+  private chartHumidity: any;
+  private chartTemperature: any;
   stationrData: StationData;
-  data = {
-    'type': "serial",
-      "theme": "dark",
-      "dataProvider": [],
-      "valueAxes": [{
-          "axisAlpha": 0,
-          "position": "left"
-      }],
-      "graphs": [{
-          "id":"g1",
-          "balloonText": "[[category]]<br><b><span style='font-size:14px;'>[[value]]</span></b>",
-          "bullet": "round",
-          "bulletSize": 8,
-          "lineColor": "#d1655d",
-          "lineThickness": 2,
-          "negativeLineColor": "#637bb6",
-          "type": "smoothedLine",
-          "valueField": "value"
-      }],
-      "chartScrollbar": {
-          "graph":"g1",
-          "gridAlpha":0,
-          "color":"#888888",
-          "scrollbarHeight":55,
-          "backgroundAlpha":0,
-          "selectedBackgroundAlpha":0.1,
-          "selectedBackgroundColor":"#888888",
-          "graphFillAlpha":0,
-          "autoGridCount":true,
-          "selectedGraphFillAlpha":0,
-          "graphLineAlpha":0.2,
-          "graphLineColor":"#c2c2c2",
-          "selectedGraphLineColor":"#888888",
-          "selectedGraphLineAlpha":1
+  chartHumidityConfig = {
+  "type": "serial",
+    "theme": "light",
+    "marginRight": 80,
+    "valueAxes": [{
+        "position": "left",
+        "title": "Umidade "
+    }],
+    "graphs": [{
+        "id": "g1",
+        "fillAlphas": 0.4,
+        "valueField": "humidity",
+         "balloonText": "<div style='margin:5px; font-size:19px;'>Umidade:<b>[[humidity]]</b></div>"
+    }],
+    "chartScrollbar": {
+        "graph": "g1",
+        "scrollbarHeight": 80,
+        "backgroundAlpha": 0,
+        "selectedBackgroundAlpha": 0.1,
+        "selectedBackgroundColor": "#888888",
+        "graphFillAlpha": 0,
+        "graphLineAlpha": 0.5,
+        "selectedGraphFillAlpha": 0,
+        "selectedGraphLineAlpha": 1,
+        "autoGridCount": true,
+        "color": "#AAAAAA"
+    },
+    "chartCursor": {
+        "categoryBalloonDateFormat": "HH mm DD MMMM",
+        "cursorPosition": "mouse"
+    },
+    "categoryField": "savedAt",
+    "categoryAxis": {
+        "minPeriod": "mm",
+        "parseDates": true
+    },
+    "dataProvider": [ ]
+  } ;
 
-      },
-      "chartCursor": {
-          "categoryBalloonDateFormat": "YYYY",
-          "cursorAlpha": 0,
-          "valueLineEnabled":true,
-          "valueLineBalloonEnabled":true,
-          "valueLineAlpha":0.5,
-          "fullWidth":true
-      },
-      "dataDateFormat": "YYYY",
-      "categoryField": "year",
-      "categoryAxis": {
-          "minPeriod": "YYYY",
-          "parseDates": true,
-          "minorGridAlpha": 0.1,
-          "minorGridEnabled": true
-      }
-  };
+  chartTemperatureConfig = {
+  "type": "serial",
+    "theme": "light",
+    "marginRight": 80,
+    "valueAxes": [{
+        "position": "left",
+        "title": "Umidade "
+    }],
+    "graphs": [{
+        "id": "g1",
+        "fillAlphas": 0.4,
+        "valueField": "temperature",
+         "balloonText": "<div style='margin:5px; font-size:19px;'>Temperatura:<b>[[temperature]]</b></div>"
+    }],
+    "chartScrollbar": {
+        "graph": "g1",
+        "scrollbarHeight": 80,
+        "backgroundAlpha": 0,
+        "selectedBackgroundAlpha": 0.1,
+        "selectedBackgroundColor": "#888888",
+        "graphFillAlpha": 0,
+        "graphLineAlpha": 0.5,
+        "selectedGraphFillAlpha": 0,
+        "selectedGraphLineAlpha": 1,
+        "autoGridCount": true,
+        "color": "#AAAAAA"
+    },
+    "chartCursor": {
+        "categoryBalloonDateFormat": "DD MMMM",
+        "cursorPosition": "mouse"
+    },
+    "categoryField": "savedAt",
+    "categoryAxis": {
+        "minPeriod": "mm",
+        "parseDates": true
+    },
+    "dataProvider": [ ]
+  } ;
   constructor(
     private route: ActivatedRoute, 
     private AmCharts: AmChartsService,
@@ -83,18 +108,20 @@ export class StationDetailsComponent implements OnInit {
       }
     });
 
-    this.chart = this.AmCharts.makeChart("chartdiv", this.data);
   }
-  
 
   retrieveData() {
     this.stationService.getByID(this.device_id).then((stData: StationData) => {
       console.log(stData);
-      this.data.dataProvider = stData.data;
+      this.chartHumidityConfig.dataProvider = <any>stData.data;//<any>this.generateChartData();//<any>stData.data;
+      this.chartTemperatureConfig.dataProvider = <any>stData.data
+      this.chartHumidity = this.AmCharts.makeChart("chartdiv", this.chartHumidityConfig);
+      this.chartTemperature = this.AmCharts.makeChart("chartTemperature", this.chartTemperatureConfig);
     })
   }
   OnDestroy() {
-    this.AmCharts.destroyChart(this.chart);
+    this.AmCharts.destroyChart(this.chartHumidity);
+    this.AmCharts.destroyChart(this.chartTemperature);
     this.sub.unsubscribe();
   }
 
